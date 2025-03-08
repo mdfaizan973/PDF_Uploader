c20576e3a32c73f2cdefca33d4e330243cec1e47
mdfaizan973 <faizan.md9735@gmail.com>
2025-03-08 16:42:43 +0530

    onig cage

---
+++ frontends/src/configs/config.js
@@ -0,0 +1,6 @@
+export const baseUrl = "https://docsdrop.vercel.app/api"; // http://localhost:5000/api
+// export const baseUrl = "https://docsdrop.vercel.app/api";
+
+// api key
+export const API_KEY =
+  "J9XqP7ZkY3LgN5V2TBR6ChQ84WmKxDFoUCMtA1EGBzvpNdWYmdFaizan973";
