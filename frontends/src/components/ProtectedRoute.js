122823b23f19d7b9a4aca7d996cd1abd11e4c5e7
mdfaizan973 <faizan.md9735@gmail.com>
2025-03-08 16:26:38 +0530

    navchnage

--- frontends/src/components/ProtectedRoute.js
+++ frontends/src/components/ProtectedRoute.js
@@ -12,12 +12,10 @@ export const ProtectedRoute = ({ children }) => {
   return children;
 };
 
-// Protected Route for the admin role_id-1 getUserInfo("role_id") will give the role_id 
+// Protected Route for the admin role_id-1 getUserInfo("role_id") will give the role_id
 export const AdminProtectedRoute = ({ children }) => {
   const roleId = getUserInfo("role_id"); // Fetch role ID
   const userDetails = JSON.parse(localStorage.getItem("userDetails"));
-  
-  console.log("Role ID:", roleId); // Debugging log
 
   // Ensure only users with roleId === 1 can access
   if (roleId !== 1) {
